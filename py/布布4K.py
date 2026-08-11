cd10d87df43fc54aa819d6cc3fb3272caf578238
tao982850 <742228696@qq.com>
2026-08-12 07:38:43 +0800

    Update 布布4K.py

--- py/布布4K.py
+++ py/布布4K.py
@@ -28,7 +28,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 class Spider(Spider):
     # 默认也放这里兜底；实际建议在配置 ext.site 里写全。
-    # 例如："site": "https://bubutv.top,https://duoduozhuiju.com,https://bbys.app,https://www.bbys.app"
+    # 例如："site": "https://bubutv.top,https://xds2435u23422342342u.top/,https://duoduozhuiju.com,https://bbys.app,https://www.bbys.app"
     DEFAULT_SITES = 'https://bubutv.top,https://duoduozhuiju.com,https://bbys.app,https://www.bbys.app'
     host = 'https://duoduozhuiju.com'
     sites = []
